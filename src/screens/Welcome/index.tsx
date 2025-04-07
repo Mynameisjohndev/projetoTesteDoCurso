bad82f098462ffec4d0219a43affb1b30f713655
Mynameisjohndev <joaoantonio15161516@gmail.com>
2025-04-07 13:58:02 -0300

    feat: configuring styled-components in our application

--- src/screens/Welcome/index.tsx
+++ src/screens/Welcome/index.tsx
@@ -1,61 +1,23 @@
-import {Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Image} from 'react-native';
 import image from '../../assets/imgs/image.png';
+import { Button, Container, Content, SubTitle, TextButton, Title } from './styles';
 
 const Welcome = () => {
   return (
-    <SafeAreaView style={styles.container}>
+    <Container>
       <Image source={image} />
-      <View style={styles.content}>
-        <Text style={styles.title}>BEM-VINDO</Text>
-        <Text style={styles.subtitle}>
+      <Content>
+        <Title>BEM-VINDO</Title>
+        <SubTitle>
           Para aprender mais, continue assistindo as aulas e praticando para
           aprender todo conteúdo!
-        </Text>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.textButton}>VAMOS LÁ</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+        </SubTitle>
+        <Button>
+          <TextButton>VAMOS LÁ</TextButton>
+        </Button>
+      </Content>
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    padding: 20,
-  },
-  content:{
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    width: '90%',
-  },
-  title: {
-    color: 'black',
-    fontWeight:'bold',
-    fontSize: 60,
-  },
-  subtitle: {
-    color: '#8C8C8E',
-    fontSize: 20,
-  },
-  button:{
-    backgroundColor: '#FFB701',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 50,
-    width: 200,
-    borderRadius: 25,
-    marginTop: 60,
-  },
-  textButton: {
-    color: '#ffffff',
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-});
 
 export default Welcome;
