ccdb820f9bf783d54cad3a5878a3b1c4c6d4a019
Mynameisjohndev <joaoantonio15161516@gmail.com>
2025-04-09 19:49:24 -0300

    feat: using hook useState

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,28 @@
 import {Button, Container, Content, TextButton, Title} from './styles';
 import { AppScreenProps } from '../../types/navigation';
+import { useState } from 'react';
+import { TextInput } from 'react-native';
 
 const Home: React.FC<AppScreenProps<'home'>> = ({navigation, route}) => {
-  const {goBack} = navigation;
+  // const {goBack} = navigation;
   const {params} = route;
+  const [name, setName] = useState<string>('');
 
   console.log(params);
 
   return (
     <Container>
       <Content>
-        <Title>Home</Title>
+        <Title>{name}</Title>
         {/* <Button onPress={() => navigate('welcome')}> */}
-        <Button onPress={() => goBack()}>
+        {/* <Button onPress={() => goBack()}>
           <TextButton>VAMOS LÁ</TextButton>
-        </Button>
+        </Button> */}
+        <TextInput
+          style={{backgroundColor: '#FF0000', width: '60%'}}
+          value={name}
+          onChangeText={setName}
+        />
       </Content>
     </Container>
   );
